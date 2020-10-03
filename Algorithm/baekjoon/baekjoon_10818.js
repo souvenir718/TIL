9f8cf749749e6640512f97a74a785bd7c610eb02
souvenir718 <mongkey0@naver.com>
2020-10-03 16:56:09 +0900

    [1003] 알고리즘 - 백준(10818:최소,최대)

---
+++ Algorithm/baekjoon/baekjoon_10818.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let time = 0;
+rl.on("line", function (line) {
+  input.push(line);
+  time++;
+  if (time === 2) rl.close();
+}).on("close", function () {
+  const input_array = input[1].split(" ").map(Number);
+  input_array.sort(function (a, b) {
+    // 오름차순
+    return a - b;
+  });
+  //   let tempNum = 0;
+  //   for (let i = 0; i < Number(input[0]); i++) {
+  //     for (let j = i; j < Number(input[0]); j++) {
+  //       if (input_array[i] > input_array[j]) {
+  //         tempNum = input_array[i];
+  //         input_array[i] = input_array[j];
+  //         input_array[j] = tempNum;
+  //       }
+  //     }
+  //   }
+  console.log(input_array[0], input_array[Number(input[0]) - 1]);
+});
