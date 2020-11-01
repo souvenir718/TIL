ec4c65e934d6842d594544ec97eb92ffe7f9f45e
souvenir718 <mongkey0@naver.com>
2020-11-02 08:41:21 +0900

    [1102] 오늘 할 일 작성

--- Algorithm/baekjoon/baekjoon_1546.js
+++ Algorithm/baekjoon/baekjoon_1546.js
@@ -1,30 +1,31 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 let input = [];
 let time = 0;
 rl.on("line", function (line) {
-  input.push(line);
-  time++;
-  if (time === 2) rl.close();
+    input.push(line);
+    time++;
+    if (time === 2) rl.close();
 }).on("close", function () {
- const size = input[0];
- input.shift();
- let scoreArr = input[0].split(" ");
-const MaxScore = Math.max.apply(null, scoreArr);
+    const size = input[0];
+    input.shift();
+    let scoreArr = input[0].split(" ");
+    const MaxScore = Math.max.apply(null, scoreArr);
 
-scoreArr = scoreArr.map(score => parseFloat(score/MaxScore * 100).toFixed(2));
+    scoreArr = scoreArr.map((score) =>
+        parseFloat((score / MaxScore) * 100).toFixed(2)
+    );
 
-const sum = scoreArr.reduce(function add(sum, cur){
-    return sum + Number(cur);
-}, 0.0);
-const avg = sum / size;
- console.log(avg);
+    const sum = scoreArr.reduce(function add(sum, cur) {
+        return sum + Number(cur);
+    }, 0.0);
+    const avg = sum / size;
+    console.log(avg);
 });
-
 
 // Math.ceil(score/MaxScore * 100))
