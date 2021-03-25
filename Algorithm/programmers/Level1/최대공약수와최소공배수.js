28418945820241076da7dfee3533d1e3d96a3e7b
souvenir718 <mongkey0@naver.com>
2021-03-26 08:16:13 +0900

    [0326] Algorithm - 최대공약수와최소공배수(프로그래머스 Lv.1)

---
+++ Algorithm/programmers/Level1/최대공약수와최소공배수.js
@@ -0,0 +1,9 @@
+function solution(n, m) {
+    var answer = [];
+    const gcd = (a, b) => {
+        if (b === 0) return a;
+        return gcd(b, a % b);
+    };
+    const lcm = (a, b) => (a * b) / gcd(a, b);
+    return [gcd(n, m), lcm(n, m)];
+}
