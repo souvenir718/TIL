4ea8746063e9baac69a9447310baafbf18aa0085
souvenir718 <mongkey0@naver.com>
2021-04-01 08:16:14 +0900

    [0401] Algorithm - 정수 내림차순으로 배치하기(프로그래머스 Lv.1)

---
+++ Algorithm/programmers/Level1/정수내림차순으로배치하기.js
@@ -0,0 +1,14 @@
+/*
+    문제
+    함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+*/
+
+function solution(n) {
+    return parseInt(
+        n
+            .toString()
+            .split('')
+            .sort((a, b) => (a < b) - (a > b))
+            .join('')
+    );
+}
