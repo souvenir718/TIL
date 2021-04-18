5c003ec630d61c33054a8036a8e224eb3cd102c8
souvenir718 <mongkey0@naver.com>
2021-04-18 22:33:35 +0900

    [0418] 오늘 할 일 끝!

--- Algorithm/programmers/Level1/시저암호.js
+++ Algorithm/programmers/Level1/시저암호.js
@@ -5,16 +5,17 @@
     문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
 */
 function solution(s, n) {
-    let result = ""; 
-    for (let i=0; i<s.length;i++) { 
-        if ( s[i] === ' ' ) 
-            result += ' ' 
-        else 
-            result += String.fromCharCode( (s.charCodeAt(i)>90)
-                    ? (s.charCodeAt(i)+n-97)%26+97 
-                    : (s.charCodeAt(i)+n-65)%26+65 ) 
-    } 
-    return result;
+  let result = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") result += " ";
+    else
+      result += String.fromCharCode(
+        s.charCodeAt(i) > 90
+          ? ((s.charCodeAt(i) + n - 97) % 26) + 97
+          : ((s.charCodeAt(i) + n - 65) % 26) + 65
+      );
+  }
+  return result;
 }
 
 //// 다시 풀어보기!!
