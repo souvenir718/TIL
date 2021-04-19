c504f6c88b3ff176d8c984c4c54ef14ee94018ba
souvenir718 <mongkey0@naver.com>
2021-04-20 05:42:45 +0900

    [0420] Algorithm - 3진법뒤집기(프로그래머스 Lv.1)

---
+++ Algorithm/programmers/Level1/3진법뒤집기.js
@@ -0,0 +1,8 @@
+/*
+    문제
+    자연수 n이 매개변수로 주어집니다.
+    n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+*/
+function solution(n) {
+  return Number.parseInt(n.toString(3).split("").reverse().join(""), 3);
+}
