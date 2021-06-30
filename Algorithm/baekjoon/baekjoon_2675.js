ad3dc396443554f63ce86684c1443a0acff3f57a
souvenir718 <mongkey0@naver.com>
2021-06-30 21:51:29 +0900

    [0630] Algorithm - 문자열반복(백준)

---
+++ Algorithm/baekjoon/baekjoon_2675.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const repeatNum = parseInt(line.split(' ').slice(0));
+  const repeatStr =line.split(' ').slice(1).toString();
+  let output ='';
+  for(let i=0; i< repeatStr.length; i++){
+    for(let j=0; j<repeatNum; j++){
+      output += repeatStr[i];
+    }
+  }
+  console.log(output);
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
