a6a05a6286a0d8255554cf8560997dfa5f430df1
souvenir718 <mongkey0@naver.com>
2021-07-06 08:25:19 +0900

    [0706] Algorithm - 단어의개수(백준)

---
+++ Algorithm/baekjoon/baekjoon_1152.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  console.log(line.split(" ").length);
+
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
