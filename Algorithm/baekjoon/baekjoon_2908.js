4d3c8788ce094b6e0f83dcd6c379f75d1b6d10cb
souvenir718 <mongkey0@naver.com>
2021-07-11 15:32:53 +0900

    [0711] 오후 한 일 체크

--- Algorithm/baekjoon/baekjoon_2908.js
+++ Algorithm/baekjoon/baekjoon_2908.js
@@ -9,7 +9,7 @@ rl.on("line", function (line) {
     let input = line.split(" ");
     let output = input.map((a) => parseInt(a.split("").reverse().join("")));
     console.log(Math.max(...output));
-    rl.close(output);
+    rl.close();
 }).on("close", function () {
     process.exit();
 });
