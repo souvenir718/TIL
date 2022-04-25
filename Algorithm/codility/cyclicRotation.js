617534cb14705030e4ce47495fed40344e8c6ae3
souvenir718 <mongkey0@naver.com>
2022-04-25 18:49:54 +0900

    [0425] Algorithm - cyclicRotation(Codility)

---
+++ Algorithm/codility/cyclicRotation.js
@@ -0,0 +1,24 @@
+/**
+ * 처음으로 푼 코드 : 75%
+ */
+function solution(A, K) {
+  if (K <= 1) return A;
+  let arr = A;
+  for (let i = 0; i < K; i++) {
+    arr.unshift(arr.pop());
+  }
+  return arr;
+}
+
+/**
+ * 예외처리
+ */
+
+function solution(A, K) {
+  if (A.length === 0) return [];
+  let arr = A;
+  for (let i = 0; i < K; i++) {
+    arr.unshift(arr.pop());
+  }
+  return arr;
+}
