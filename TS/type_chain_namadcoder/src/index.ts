0a38102727b889f8a19f2a7de492a3f0c2162beb
souvenir718 <mongkey0@naver.com>
2022-06-02 07:25:29 +0900

    [0602] TS - Blocks(nomadcoder)

--- TS/type_chain_namadcoder/src/index.ts
+++ TS/type_chain_namadcoder/src/index.ts
@@ -1,8 +1,32 @@
-import { init, exit } from "./myPackage";
+// import { init, exit } from "./myPackage";
 
-init({
-  debug: true,
-  url: "test",
-});
+// init({
+//   debug: true,
+//   url: "test",
+// });
 
-exit(3);
+// exit(3);
+
+import crypto from "crypto";
+
+interface BlockShpae {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShpae {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+  }
+}
